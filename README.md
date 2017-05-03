b2f06075932c8e41a6d75cee6416f7aa0f004d8d
altreact <alt.react@gmail.com>
2017-05-03 11:14:17 -0400

    screen brightness controls for arch linux

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 #arch_blctl
-
+ 
+* works on Samsung ARM Chromebook
+ 
 replace $b's value with the path of the file that keeps your backlight's value
 replace $mb's value with the path of the file that keeps your backlight's max_brightness value
 replace $incr's value with the number of steps you'd like the script to alter your brightness level, each time the script is ran
 
-"sudo mv b /usr/local/bin/b"
-"sudo chmod +x /usr/local/bin/b"
-
+"sudo sh bri_ctrl.sh"
+ 
 "b u" to increase backlight brightness
 "b d" to decrease backlight brightness
+ 
+the commands above can then be mapped to keys on your keyboard
---
+++ bri_ctr.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+if [ "$(whoami)" != 'root' ]; then
+  echo
+  echo 'script must be ran as root'
+  echo
+  exit 1
+fi
+
+echo 'f /sys/class/backlight/backlight.12/brightness 0666 - - - 800' > /etc/tmpfiles.d/brightness.conf
+
+echo 'usr/bin/env bash
+
+b="/sys/class/backlight/backlight.12/brightness"
+mb="/sys/class/backlight/backlight.12/max_brightness"
+cur_bri="$(cat $b)"
+maxb="$(cat $mb)" 
+incr=100
+
+if [ $1 = 'u' ] && [ `expr $cur_bri + $incr` -le $maxb ]; then
+    echo `expr $cur_bri + $incr` > $b
+fi
+if [ $1 = 'd' ] && [ `expr $cur_bri - $incr` -ge 0 ]; then
+    echo `expr $cur_bri - $incr` > $b
+fi' > /usr/local/bin/b
+
+chmod +x /usr/local/bin/b
+
+echo
+echo 'system must be rebooted for changes to take effect'
+echo
+read -p 'reboot now? [N/y]: ' a
+
+if [ $a = 'y' ]; then
+  reboot
+fi
